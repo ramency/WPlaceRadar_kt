0d9064b46549877d7a67bffd80843b13509dcec1
ramency <dav.szanto@gmail.com>
2025-08-28 16:39:51 +0200

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,35 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.HttpMethod
+import io.ktor.util.cio.writeChannel
+import io.ktor.utils.io.ByteReadChannel
+import io.ktor.utils.io.copyAndClose
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import java.io.File
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 suspend fun main() {
     val client = HttpClient(CIO)
 
-    getMapTile(client, 100, 299)
+    SystemFileSystem.createDirectories(Path("/mapTiles"), false) // create new folder for the map pngs
+
+    getMapTile(client, 100, 299) // example to test function
 
     client.close()
 }
 
 suspend fun getMapTile(client: HttpClient, tileX: Int, tileY: Int) {
-    val response: HttpResponse = client.get("https://backend.wplace.live/files/s0/tiles/$tileX/$tileY.png")
+    val timeAndDate = java.time.LocalDateTime.now().toString()
+
+    val mapTilePNG = File("MapTile-$tileX-$tileY---$timeAndDate")
+    val out: ByteReadChannel = client.get {
+        url("https://backend.wplace.live/files/s0/tiles/$tileX/$tileY.png")
+        method = HttpMethod.Get
+    }.bodyAsChannel()
+
+   out.copyAndClose(mapTilePNG.writeChannel())
 }
